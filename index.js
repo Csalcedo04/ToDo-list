7528bb63da61f591f30454b084db171aca4657c2
Carlos Alberto Salcedo Rodriguez <csalcedo@utb.edu.co>
2023-11-13 14:12:10 -0500

    se completa entrega

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import pg from "pg";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app= express();
 const port = 3000;
@@ -17,7 +16,7 @@ const db = new pg.Client({
   });
   db.connect();
 
-// var toDo = [];
+  var local_toDo = [];
 var workToDo = [];
 
 
@@ -37,21 +36,22 @@ var today = days[day-1] + ", "+date+" "+months[month];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"))
-app.use(tasks())
+
 async function tasks (){
     const result = await db.query("SELECT todo_task From today")
     var toDo = [];
     result.rows.forEach((tarea)=>{
         toDo.push(tarea.todo_task);
     });
+    local_toDo= toDo;
     return toDo;
 }
 
 app.get("/", async (req, res) =>{
-    var numberOfTask = toDo.length;
     var datos = await tasks();
+    var numberOfTask = datos.length;
     var Data = {
-        todo: toDo,
+        todo: datos,
         days: today,
         ntasks: numberOfTask
     };
@@ -59,49 +59,58 @@ app.get("/", async (req, res) =>{
 })
 
 app.post("/submit", async (req,res)=>{
-    var newTask={
-        task: req.body["task"],
-    }
+    var datos = await tasks();
     var tarea = req.body["task"];
     await db.query("INSERT INTO today (todo_task) VALUES ($1)",[tarea])
-    toDo.push(newTask)
-    
-    
-    var numberOfTask = toDo.length;
+
+    var numberOfTask = datos.length;
     var Data = {
-        todo: toDo,
-        task: newTask,
+        todo: datos,
+        task: tarea,
         days: today,
         ntasks: numberOfTask
     };
     res.render(__dirname+"/views/index.ejs", Data) 
 })
 
+async function myday (){
+    const result = await db.query("SELECT todo_task From work_to_do")
+    var work_to_do = [];
+    result.rows.forEach((tarea)=>{
+        work_to_do.push(tarea.todo_task);
+    });
+    workToDo= work_to_do;
+    return work_to_do;
+}
 
-app.get("/my-day", (req,res)=>{
+app.get("/my-day", async (req,res)=>{
     var numberOfTask = workToDo.length;
+    var wtd = await myday();
     var Data = {
-        work: workToDo,
+        work: wtd,
         wtasks: numberOfTask
     };
     res.render(__dirname+"/views/work.ejs", Data)
 })
 app.post("/worktodo",async (req,res)=>{
-    var newTask=req.body["task_checkbox"]
-    workToDo.push(newTask)
-    var numberOfTask = workToDo.length;
+    var tarea = req.body["work_task"];
+    var wtd = await myday();
+    var numberOfTask = wtd.length;
     var Data = {
-        work: workToDo ,
-        new_task: newTask,
+        work: wtd ,
+        new_task: tarea,
         wtasks: numberOfTask
     };
-    var tarea = req.body["task_checkbox"];
     await db.query("INSERT INTO work_to_do (todo_task) VALUES ($1)",[tarea])
+    
     res.render(__dirname+"/views/work.ejs", Data) 
 })
 
-// app.delete("/" ,(req, res)=>{
-//     var delete_task = req.body["checkbox"].id 
+// app.delete("/submit" , async (req, res)=>{
+//     var delete_task = req.body["task"]
+//     await db.query("DELETE FROM today WHERE todo_task = $1", [delete_task])
+//     res.render(__dirname+"/views/index.ejs")
+
 // })
 
 app.listen(port, ()=>{
